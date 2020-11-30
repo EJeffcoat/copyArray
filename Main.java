1701d41c0887c2bb37d875d150faae41b3e367f5
Ethan Jeffcoat <ethan.jeffcoat@patelhs.org>
2020-11-30 15:41:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class copyArray {
+  public static void main(String[] args) {
+   String roster1[] = {"Ethan", "Gabe", "Thomas", "Liam", "Caden", "Christian", "Hunter", "Jack", "Tommy", "Luis"};
+   String[] roster2 = {};
+}
+  }
+}
